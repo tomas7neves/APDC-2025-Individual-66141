f7109b6a7202409dc88d79452cbac40897f628c5
AfonsoVi <j.vilalonga@campus.fct.unl.pt>
2024-03-13 20:58:48 +0000

    commit

---
+++ src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/ComputationResource.java
@@ -0,0 +1,75 @@
+package pt.unl.fct.di.apdc.firstwebapp.resources;
+
+import java.io.IOException;
+import java.net.URI;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.Clock;
+import java.time.Instant;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+
+import com.google.cloud.tasks.v2.*;
+import com.google.gson.Gson;
+import com.google.protobuf.Timestamp;
+
+
+@Path("/utils")
+@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8") 
+public class ComputationResource {
+
+	private static final Logger LOG = Logger.getLogger(ComputationResource.class.getName()); 
+	private final Gson g = new Gson();
+
+	private static final DateFormat fmt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSZ");
+
+	public ComputationResource() {} //nothing to be done here @GET
+
+	@GET
+	@Path("/hello")
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response hello() throws IOException{
+		try {
+			throw new IOException("UPS");
+		} catch (Exception e) {
+			LOG.log(Level.SEVERE, "Exception on Method /hello", e);
+			return Response.temporaryRedirect(URI.create("/error/500.html")).build();
+		}
+	}
+	
+	@GET
+	@Path("/time")
+	public Response getCurrentTime() {
+
+		LOG.fine("Replying to date request.");
+		return Response.ok().entity(g.toJson(fmt.format(new Date()))).build();
+	}
+	
+	@GET
+	@Path("/compute")
+	public Response triggerExecuteComputeTask() throws IOException {
+		String projectId = "quantum-shard-415522";
+		String queueName = "Default";
+		String location = "europe-west6";
+		LOG.log(Level.INFO, projectId + " :: " + queueName + " :: " + location );
+
+		try (CloudTasksClient client = CloudTasksClient.create()) {
+			String queuePath = QueueName.of(projectId, location, queueName).toString();
+			Task.Builder taskBuilder = Task.newBuilder().setAppEngineHttpRequest(AppEngineHttpRequest.newBuilder()
+							.setRelativeUri("/rest/utils/compute").setHttpMethod(HttpMethod.POST).build());
+
+			taskBuilder.setScheduleTime(Timestamp.newBuilder().setSeconds(Instant.now(Clock.systemUTC()).getEpochSecond()));
+			
+			client.createTask(queuePath, taskBuilder.build());
+		} 
+		return Response.ok().build();
+	}
+}
---
+++ src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/LoginResource.java
@@ -0,0 +1,58 @@
+package pt.unl.fct.di.apdc.firstwebapp.resources;
+
+import java.util.logging.Logger;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import pt.unl.fct.di.apdc.firstwebapp.util.AuthToken;
+import pt.unl.fct.di.apdc.firstwebapp.util.LoginData;
+
+import com.google.gson.Gson;
+
+
+@Path("/login")
+@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+public class LoginResource {
+
+	
+	private static final Logger LOG = Logger.getLogger(LoginResource.class.getName());
+	private final Gson g = new Gson();
+	
+	public LoginResource() {
+		
+	}
+	
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response doLogin(LoginData data) {
+		LOG.fine("Login attempt by user: " + data.username);
+		
+		if(data.username.equals("jleitao") && data.password.equals("password")) { 
+			AuthToken at = new AuthToken(data.username);
+			return Response.ok(g.toJson(at)).build();
+		}
+		
+		return Response.status(Status.FORBIDDEN).entity("Incorrect username or password.").build();
+
+	}
+	
+	@GET
+	@Path("/{username}")
+	public Response checkUsernameAvailable(@PathParam("username") String username) {
+		if(username.equals("jleitao")) {
+			return Response.ok().entity(g.toJson(false)).build();
+		} else {
+			return Response.ok().entity(g.toJson(true)).build();
+		}
+	}
+
+}
